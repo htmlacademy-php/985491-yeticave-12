5f583633a91f7f8467f1710388c80f36b55a14f4
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2020-07-02 13:40:25 +0500

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $user_name = 'Mikhail'; // укажите здесь ваше имя
         <nav class="user-menu">
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-            <?php if ($is_auth == 1) : ?>
+            <?php if ($is_auth === 1) : ?>
             <div class="user-menu__logged">
             <p><?=$user_name;?></p>
             <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
