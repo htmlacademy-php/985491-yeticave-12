4f9b4be2d6dcfc777d3bfe0efb51b962319d3fb0
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2020-07-04 14:34:59 +0500

    Итерируем по массивам

--- index.php
+++ index.php
@@ -56,9 +56,14 @@ $user_name = 'Mikhail'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+            <?php
+            $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+            ?>
+            <?php foreach ($categories as $value): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$value; ?></a>
             </li>
+        <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -67,17 +72,58 @@ $user_name = 'Mikhail'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php
+            $product = [
+                [
+                'name' => "2014 Rossingnol District Snowboard",
+                'categorie' => $categories[0],
+                'price' => 10999,
+                'URL_pict' => "img/lot-1.jpg"
+                ],
+                [
+                'name' => "DC Ply Mens 2016/2017 Snowboard",
+                'categorie' => $categories[0],
+                'price' => 159999,
+                'URL_pict' => "img/lot-2.jpg"
+                ],
+                [
+                'name' => "Крепления Union Contact Pro 2015 года размер L/XL",
+                'categorie' => $categories[1],
+                'price' => 8000,
+                'URL_pict' => "img/lot-3.jpg"
+                ],
+                [
+                'name' => "Ботинки для сноуборда DC Mutiny Charocal",
+                'categorie' => $categories[2],
+                'price' => 10999,
+                'URL_pict' => "img/lot-4.jpg"
+                ],
+                [
+                'name' => "Куртка для сноуборда DC Mutiny Charocal",
+                'categorie' => $categories[3],
+                'price' => 7500,
+                'URL_pict' => "img/lot-5.jpg"
+                ],
+                [
+                'name' => "Маска Oakley Canopy",
+                'categorie' => $categories[5],
+                'price' => 5400,
+                'URL_pict' => "img/lot-6.jpg"
+                ],
+            ];
+            ?>
+            <?php foreach ($product as $key => $value): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$value['URL_pict'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$value['categorie'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -85,6 +131,7 @@ $user_name = 'Mikhail'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+        <? endforeach; ?>
         </ul>
     </section>
 </main>
