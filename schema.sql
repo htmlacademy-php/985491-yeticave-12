c3c369b54e52c8c0bb11598a372ba085ce70921b
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2020-07-19 22:26:27 +0500

    Редакция 2

--- schema.sql
+++ schema.sql
@@ -11,32 +11,34 @@ CREATE TABLE categories (
   symbol_code CHAR(32) NOT NULL
 );
 
+INSERT INTO categories (name, symbol_code) VALUES ('Доски и лыжи', 'boards');
+INSERT INTO categories (name, symbol_code) VALUES ('Крепления', 'attachment');
+INSERT INTO categories (name, symbol_code) VALUES ('Ботинки', 'boots');
+INSERT INTO categories (name, symbol_code) VALUES ('Одежда', 'clothing');
+INSERT INTO categories (name, symbol_code) VALUES ('Инструменты', 'tools');
+INSERT INTO categories (name, symbol_code) VALUES ('Разное', 'other');
+
 
 CREATE TABLE lots (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   date_create TIMESTAMP NOT NULL,
   name CHAR(64) NOT NULL,
-  description TEXT,
+  description TEXT NOT NULL,
   url_image CHAR(128) NOT NULL,
   start_price INT NOT NULL,
   date_end TIMESTAMP NOT NULL,
   step_price INT NOT NULL,
-  author INT,
+  author INT NOT NULL,
   winner INT,  
-  category INT/*,
-  FOREIGN KEY (author)  REFERENCES users (id),
-  FOREIGN KEY (winner)  REFERENCES users (id),
-  FOREIGN KEY (category)  REFERENCES categories (id)*/
+  category INT NOT NULL
 );
 
 CREATE TABLE bets (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   price INT NOT NULL,
-  user INT,
-  lot INT/*,
-  FOREIGN KEY (user)  REFERENCES users (id),
-  FOREIGN KEY (lot)  REFERENCES lots (id)*/
+  user INT NOT NULL,
+  lot INT NOT NULL
 );
 
 CREATE TABLE users (
@@ -45,44 +47,19 @@ CREATE TABLE users (
   email VARCHAR(254) NOT NULL UNIQUE,
   name CHAR(64) NOT NULL,
   password TEXT NOT NULL,
-  created_lot INT,
-  bet INT/*,
-  FOREIGN KEY (created_lot)  REFERENCES lots (id),
-  FOREIGN KEY (bet)  REFERENCES bets (id)*/
+  contact TEXT NOT NULL
 );
 
-/*CREATE TABLE lot_author (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  price INT NOT NULL,
-  user INT,
-  lot INT/*,
-  FOREIGN KEY (user)  REFERENCES users (id),
-  FOREIGN KEY (lot)  REFERENCES lots (id)
-);*/
-
-/*ALTER TABLE lots ADD COLUMN author INTEGER REFERENCES users(id);*/
 
 CREATE INDEX name_category ON categories(name);
 CREATE INDEX date_create ON lots(date_create);
 CREATE INDEX name_lot ON lots(name);
 CREATE INDEX date_end ON lots(date_end);
-/*CREATE INDEX password ON users(password);*/
+CREATE INDEX email_index ON users(email);
 
 ALTER TABLE lots ADD FOREIGN KEY (author)  REFERENCES users (id);
 ALTER TABLE lots ADD FOREIGN KEY (winner)  REFERENCES users (id);
 ALTER TABLE lots ADD FOREIGN KEY (category)  REFERENCES users (id);
 ALTER TABLE bets ADD FOREIGN KEY (user)  REFERENCES users (id);
 ALTER TABLE bets ADD FOREIGN KEY (lot)  REFERENCES lots (id);
-ALTER TABLE users ADD FOREIGN KEY (created_lot)  REFERENCES lots (id);
-ALTER TABLE users ADD FOREIGN KEY (bet)  REFERENCES bets (id);
 
-  /*FOREIGN KEY (author)  REFERENCES users (id);
-  FOREIGN KEY (winner)  REFERENCES users (id);
-  FOREIGN KEY (category)  REFERENCES categories (id);
-
-  FOREIGN KEY (user)  REFERENCES users (id);
-  FOREIGN KEY (lot)  REFERENCES lots (id);
-
-  FOREIGN KEY (created_lot)  REFERENCES lots (id);
-  FOREIGN KEY (bet)  REFERENCES bets (id);*/
