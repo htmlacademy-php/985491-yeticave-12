e19180259f78a32cf407f5d43afa5e846b866b4c
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2020-10-14 22:26:08 +0500

    Поправлено

--- add.php
+++ add.php
@@ -29,62 +29,30 @@ function getPostVal(string $name): string {
 }
 
 function validation_format_date(string $date): bool {
-   $pattern = "/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/"; // Основной 2013-10-22  
-  if ( preg_match($pattern, $date, $razdeli) ) :
-if ( checkdate($razdeli[2],$razdeli[3],$razdeli[1]) )
-      return true;
-    else
+  $format_date = date_create_from_format('Y-m-j', $date);
+   if ($format_date === false){
       return false;
-  else :
-    return false;
-  endif;
+   }
+   else {
+      return true;
+   }  
 } 
 
 $added_lot = [];
 $errors_validate = [];
 $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
 
-/*function validate_form_add_lot (array $required_fields): void {  
-    foreach ($required_fields as $field) {
-      if (empty($_POST[$field])) {
-          $errors_validate[$field] = 'Поле не заполнено';
-      }
-      else {$added_lot[$field] = $_POST[$field];}
-    }    
-    if (isset($_FILES['file_img_lot'])) {
-      $file_name = $_FILES['file_img_lot']['name'];
-      $file_path = __DIR__ . '/uploads/';
-      $file_url = '/uploads/' . $file_name;
-      //$added_lot['file_img_lot'] = 'uploads/';
-      move_uploaded_file($_FILES['file_img_lot']['tmp_name'], $file_path . $file_name);
-      $url_file = (string)$file_url;
-      $added_lot['file_img_lot'] = $url_file;
-      //$added_lot['file_img_lot'] = 'uploads/';
-      //print($_FILES['file_img_lot']['type']);
-    }
-    else {      
-      $errors_validate['file_img_lot'] = 'Поле не заполнено';
-    }
-
-  if ($errors_validate) {
-       // показать ошибку валидации
-      print('Ошибка валидации ');
-      foreach ($errors_validate as $key => $err) {
-        print($key . ' => ' . $err . '; ');
-      }
-      //return false;
-  }  
-}*/
-
 $sql_read_categories = "SELECT * FROM categories";
 $result_categories = mysqli_query($connect, $sql_read_categories);
 $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
 
-$sql_read_id_category = "SELECT id FROM categories WHERE name ='" . $_POST['category'] ."'";
+
+$name_category_safe = mysqli_real_escape_string($connect, $_POST['category']);
+$sql_read_id_category = "SELECT id FROM categories WHERE name ='" . $name_category_safe ."'";
 $result_id = mysqli_query($connect, $sql_read_id_category);
 $id_select_category = mysqli_fetch_array($result_id, MYSQLI_ASSOC);
 
-if (isset($_POST['lot-name'])) {  //Если есть такое поле в POST, значит форма отправлена
+if (isset($_POST['submit'])) {  //Если есть такое поле в POST, значит форма отправлена
   foreach ($required_fields as $field) {
     if (empty($_POST[$field])) {  
       $errors_validate[$field] = 'Поле не заполнено'; 
@@ -98,48 +66,74 @@ if (isset($_POST['lot-name'])) {  //Если есть такое поле в POS
   $added_lot['author'] = 1;  
   $added_lot['date_create'] = date('Y-m-d H:i:s');
 
-  if (isset($_FILES['file_img_lot'])) {
+  if (isset($_FILES['file_img_lot']) && !empty($_FILES['file_img_lot']['name'])) {
     $file_name = $_FILES['file_img_lot']['name'];
     $file_path = __DIR__ . '/uploads/';
     $file_url = '/uploads/' . $file_name;    
-    move_uploaded_file($_FILES['file_img_lot']['tmp_name'], $file_path . $file_name);
-    $url_file = (string)$file_url;
-    $added_lot['file_img_lot'] = $url_file;
+    
+    if (file_exists($file_path . $file_name)) {   
+      $i = 1;
+      while (file_exists($file_path . $temp_file_name))   {
+        $temp_file_name = '(' . $i . ')' . $file_name;
+        $i++;
+      }      
+      $file_name = $temp_file_name;
+      $file_url = '/uploads/' . $file_name;       
+    }
+    
+    if (move_uploaded_file($_FILES['file_img_lot']['tmp_name'], $file_path . $file_name)) {
+      $url_file = (string)$file_url;
+      $added_lot['file_img_lot'] = $url_file;
+    }
+    else {
+      $errors_validate['file_img_lot_error_move'] = 'Ошибка при перемещении файла ';
+    }    
   }           
   else { 
-    $errors_validate['file_img_lot'] = 'Поле не заполнено'; 
+    $errors_validate['file_img_lot'] = 'Поле не заполнено '; 
   }
 
   //Валидация соответствующих полей и сохранение ошибок (при наличии)
+  $category_exists = 0;
+  foreach ($categories as $item_category) { //проверяется, что введенная категория существует
+    if ($item_category['name'] === $_POST['category']) {
+      $category_exists = 1;
+    }
+  }
   if ($_POST['category'] === 'Выберите категорию') {
     $errors_validate['category'] = 'Не выбрана категория';
   }
-
-  if ($_POST['lot-rate'] <= 0) {
-    $errors_validate['lot-rate'] = 'Начальная цена должна быть больше нуля';
+  if ($category_exists === 0 && $_POST['category'] != 'Выберите категорию') {
+    $errors_validate['category'] = 'Выбрана несуществующая категория';
   }
 
-  if (!ctype_digit ($_POST['lot-step'])) {
+  if ($_POST['lot-rate'] <= 0 && !$errors_validate['lot-rate']) {
+    $errors_validate['lot-rate'] = 'Начальная цена должна быть числом больше нуля';
+  }
+
+  if (!ctype_digit ($_POST['lot-step']) && !$errors_validate['lot-step']) {
     $errors_validate['lot-step_format'] = 'Шаг ставки должен быть целым числом больше нуля';
   }
     
-  if ($_POST['lot-step'] <= 0) {
+  if ($_POST['lot-step'] <= 0 && !$errors_validate['lot-step']) {
     $errors_validate['lot-step_format'] = 'Шаг ставки должен быть целым числом больше нуля';
   }    
 
-  if (!validation_format_date($_POST['lot-date'])) {
+  if (!validation_format_date($_POST['lot-date']) && !$errors_validate['lot-date']) {
     $errors_validate['lot-date_format'] = 'Дата должна быть введена в формате "ГГГГ-ММ_ДД"';
   }
 
   $hours_and_minuts_2 = get_dt_range($_POST['lot-date']); //получение массива с часами и минутами разницы времени между введенной датой окончания торгов и текущей датой
-  if ($hours_and_minuts_2[0] < 24) {  //В "0" элементе хранятся часы, если меньше 24 часов, значит менее суток
+  if ($hours_and_minuts_2[0] < 24 && !$errors_validate['lot-date']) {  //В "0" элементе хранятся часы, если меньше 24 часов, значит менее суток
     $errors_validate['lot-date_earlier_current_time'] = 'Дата окончания торгов должна быть позже текущего времени минимум на 24 часа'; 
   }
 
-  $type_file = mime_content_type($file_path . $file_name);
-  if (!($type_file === 'image/jpeg' || $type_file === 'image/png')) { 
-    $errors_validate['file_img_lot_mime'] = 'Допустимы только файлы изображений типов jpeg и png';  
-  } 
+  if (!$errors_validate['file_img_lot_error_move'] && !$errors_validate['file_img_lot']) {    
+    $type_file = mime_content_type($file_path . $file_name);
+    if (!($type_file === 'image/jpeg' || $type_file === 'image/png' || $type_file === 'image/jpg')) { 
+      $errors_validate['file_img_lot_mime'] = 'Допустимы только файлы изображений типов jpeg, jpg и png ';  
+    } 
+  }
    
   //Если были ошибки валидации - возвращаем на страницу добавления нового лота с показом ошибок
   if ($errors_validate) {   
@@ -149,16 +143,13 @@ if (isset($_POST['lot-name'])) {  //Если есть такое поле в POS
   }
   else {
     //Если ошибок не было - добавляем ноый лот в БД
-    $sql_insert_lot = "INSERT INTO lots (date_create, name, description, url_image, start_price, date_end, step_price, author, category) VALUES ('" . $added_lot['date_create'] . "', '" . $added_lot['lot-name'] . "','" . $added_lot['message'] . "','" . $added_lot['file_img_lot'] . "',' " . $added_lot['lot-rate'] . "', '" . $added_lot['lot-date'] . "', '" . $added_lot['lot-step'] . "', '" . $added_lot['author'] . "', '" . $added_lot['category'] . "')";
-    $result_insert_lot = mysqli_query($connect, $sql_insert_lot);
+    $sql_insert_lot = 'INSERT INTO lots (date_create, name, description, url_image, start_price, date_end, step_price, author, category) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
     
-    if ($result_insert_lot) { //Получаем id последней добавленной записи (только что добавленного лота)
-      $id_last_insert_lot = mysqli_insert_id($connect);
-    }
-    else{
-      $error = mysqli_error($connect); 
-     print("Ошибка MySQL: " . $error);
-    }
+    $stmt = mysqli_prepare($connect, $sql_insert_lot);
+    mysqli_stmt_bind_param($stmt, 'sssssssii', $added_lot['date_create'], $added_lot['lot-name'], $added_lot['message'], $added_lot['file_img_lot'], $added_lot['lot-rate'], $added_lot['lot-date'], $added_lot['lot-step'], $added_lot['author'], $added_lot['category']);
+    mysqli_stmt_execute($stmt);
+    $id_last_insert_lot = mysqli_insert_id($connect);
 
     //Получаем из БД данные по только что добавленному лоту
     $sql_read_lot = "SELECT lots.date_create, lots.name, lots.description, lots.url_image, lots.start_price, lots.date_end, lots.step_price, categories.name AS name_category FROM lots JOIN categories ON lots.category = categories.id WHERE lots.id ='" . $id_last_insert_lot ."'";
@@ -170,10 +161,8 @@ if (isset($_POST['lot-name'])) {  //Если есть такое поле в POS
       exit("Страница с id =" . $id . " не найдена.");
     }
 
-    //Показываем страницу с только что добавленным лотом
-    $content_page = include_template('content_lot.php', $data = ['open_lot' => $open_lot, 'categories' => $categories, 'bet_open_lot' => $bet_open_lot]);
-    $page = include_template('layout.php', $data = ['categories' => $categories, 'content_page' => $content_page, 'name_page' => htmlspecialchars($open_lot['name']) , 'user_name' => $user_name, 'is_auth' => $is_auth]);
-    print($page);    
+    //Перенаправляем на страницу с только что добавленным лотом    
+    header('Location: lot.php?id=' . $id_last_insert_lot);    
   }
 }
 else {  //Если форма не отправлена, показываем страницу добавления лота
--- templates/content_add_lot.php
+++ templates/content_add_lot.php
@@ -14,7 +14,7 @@
         <div class="form__item <?php if($errors_validate['lot-name']): ?> form__item--invalid <?php endif; ?> "> <!-- form__item--invalid -->
           <label for="lot-name">Наименование <sup>*</sup></label>
           <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="<?=getPostVal('lot-name'); ?>">
-          <span class="form__error">Не введено наименование лота</span>
+          <span class="form__error"><?=$errors_validate['lot-name'] ?></span>
         </div>
         <div class="form__item <?php if($errors_validate['category']): ?> form__item--invalid <?php endif; ?>">
           <label for="category">Категория <sup>*</sup></label>
@@ -24,43 +24,51 @@
               <option <?php if($category['name'] === getPostVal('category')): ?> selected <?php endif; ?> ><?=htmlspecialchars($category['name']) ?></option>              
             <?php endforeach; ?>                
           </select>
-          <span class="form__error">Не выбрана категория</span>
+          <span class="form__error"><?=$errors_validate['category'] ?></span>
         </div>
       </div>
       <div class="form__item form__item--wide <?php if($errors_validate['message']): ?> form__item--invalid <?php endif; ?>">
         <label for="message">Описание <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите описание лота"><?=getPostVal('message'); ?></textarea>
-        <span class="form__error">Не введено описание лота</span>
+        <span class="form__error"><?=$errors_validate['message'] ?></span>
       </div>
-      <div class="form__item form__item--file <?php if($errors_validate['file_img_lot'] || $errors_validate['file_img_lot_mime']): ?> form__item--invalid <?php endif; ?>">
+      <div class="form__item form__item--file <?php if($errors_validate['file_img_lot'] || $errors_validate['file_img_lot_mime'] || $errors_validate['file_img_lot_error_move']): ?> form__item--invalid <?php endif; ?>">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
           <input class="visually-hidden" type="file" id="lot-img" value="" name="file_img_lot">
           <label for="lot-img">
             Добавить
           </label>
-          <span class="form__error"><?php if($errors_validate['file_img_lot']): ?> Не добавлено фото товара <?php endif; ?> <?php if($errors_validate['file_img_lot_mime']): ?> Допустимы только файлы изображений типов jpeg и png <?php endif; ?></span>
+          <span class="form__error">
+            <?php if($errors_validate['file_img_lot']){ print($errors_validate['file_img_lot']);} 
+            if($errors_validate['file_img_lot_mime']){ print($errors_validate['file_img_lot_mime']);} 
+            if($errors_validate['file_img_lot_error_move']){ print($errors_validate['file_img_lot_error_move']);} ?></span>
         </div>
       </div>
       <div class="form__container-three">
         <div class="form__item form__item--small <?php if($errors_validate['lot-rate']): ?> form__item--invalid <?php endif; ?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
           <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?=getPostVal('lot-rate'); ?>">
-          <span class="form__error">Введите начальную цену. Она должна быть больше нуля</span>
+          <span class="form__error"><?=$errors_validate['lot-rate'] ?></span>
         </div>
         <div class="form__item form__item--small <?php if($errors_validate['lot-step'] || $errors_validate['lot-step_format']): ?> form__item--invalid <?php endif; ?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
           <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?=getPostVal('lot-step'); ?>" >
-          <span class="form__error"><?php if($errors_validate['lot-step']): ?> Не введен шаг ставки <?php endif; ?> <?php if($errors_validate['lot-step_format']): ?> Шаг ставки должен быть целым числом больше нуля <?php endif; ?></span>
+          <span class="form__error">
+            <?php if($errors_validate['lot-step']) { print($errors_validate['lot-step']);} 
+            if($errors_validate['lot-step_format']) { print($errors_validate['lot-step_format']);} ?></span>
         </div>
         <div class="form__item <?php if($errors_validate['lot-date'] || $errors_validate['lot-date_earlier_current_time'] || $errors_validate['lot-date_format']): ?> form__item--invalid <?php endif; ?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
           <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?=getPostVal('lot-date'); ?>" >
-          <span class="form__error"><?php if($errors_validate['lot-date']): ?> Не введена дата завершения торгов <?php endif; ?> <?php if($errors_validate['lot-date_earlier_current_time']): ?> Дата окончания торгов должна быть позже текущего времени минимум на 24 часа <?php endif; ?><?php if($errors_validate['lot-date_format']): ?> Дата должна быть введена в формате "ГГГГ-ММ_ДД <?php endif; ?></span>
+          <span class="form__error">
+            <?php if($errors_validate['lot-date']) { print($errors_validate['lot-date']);} 
+            if($errors_validate['lot-date_earlier_current_time']) { print($errors_validate['lot-date_earlier_current_time']);}  
+            if($errors_validate['lot-date_format']){ print($errors_validate['lot-date_earlier_current_time']);} ?></span>
         </div>
       </div>
       <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
-      <button type="submit" class="button">Добавить лот</button>
+      <button type="submit" name="submit" class="button">Добавить лот</button>
     </form>
   </main>
 
